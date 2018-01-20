5882781c9d82e3cd80fce2863115541f928e889b
Kelly Yamamoto <minedamnesia@gmail.com>
2018-01-19 22:54:12 -0500

    update functional tests for new TDD steps

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,6 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
@@ -13,18 +15,34 @@ class NewVisitorTest(unittest.TestCase):
 	    # Kelly has heard about a new online todo app.  She checks out its homepage
         self.browser.get('http://localhost:8000')
 
-    	# She looks for the page title and header mentioning todo lists
-    	self.assertIn('To-Do', self.browser.title)
-    	self.fail('Finish the test!')
+       # She looks for the page title and header mentioning todo lists
+        self.assertIn('To-Do', self.browser.title)
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
 
         # She is invited to enter a to do item
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
 
         # She types 'Buy peacock feathers' into a text box
+        inputbox.send_keys('Buy peacock feathers')
 
         # When she hits enter, the page updates and the page lists her task as an item in the todo list
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_element_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: Buy peacock feathers' for row in rows)
+        )
 
         # There is a text box asking her to add another item.
         # She enters 'use peackcok deathers to make a fly'
+        self.fail('Finish the test!')
 
         # The page updates again and shows both items on her list
 
